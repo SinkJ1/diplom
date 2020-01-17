1f9a93ca58afa93f2aeadf8d81491898e35a5206
SinkJ <boi54357@gmail.com>
2020-01-17 23:13:30 +0300

    start adding navigation

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import Header from './components/Header';
 import Body from './components/Body';
 import Basement from './components/Basement';
 import Menu from './components/SlideMenu';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+const main = () => {
+  return (<><Header />
+    <Menu />
+    <Body />
+    <Basement />
+    </>);
+}
+
+const about = () => {
+  return (<div>lkjsdag;lkjn</div>);
+}
 
 function App() {
 
 
-
   return (
     <>
-    <Header />
-    <Menu />
-    <Body />
-    <Basement />
-    </>
+    <Router>
+            <Switch>
+                <Route path="/main" component={main} />
+                <Route path="/about" component={about} />
+            </Switch>
+        </Router>
+        </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import "./styles/header.css"
 import Window from './Window';
+import HeaderMenu from './HeaderMenu';
 
 
 const Header = () =>{
@@ -16,6 +17,8 @@ const Header = () =>{
     }
     
     return (<div id="header">
+        <div className="logo">LOGO</div>
+        <HeaderMenu />
         <button className="buttonIn" onClick={o}> Вход </button>
         <input type="text"className="findPlace"/>
         <button className="buttonReg" onClick={a}> Регистрация </button>
