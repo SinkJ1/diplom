5c3cdd56ecfee6019b3b9b3af64958703d14c0e9
SinkJ <boi54357@gmail.com>
2020-01-17 12:19:32 +0300

    change input button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Body from './components/Body';
 import Basement from './components/Basement';
+import Menu from './components/SlideMenu';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
     <Header />
+    <Menu />
     <Body />
     <Basement />
     </>
