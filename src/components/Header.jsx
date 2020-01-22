a886a61cd3979d4a1e699ef007fd4fb241ef7da6
SinkJ <boi54357@gmail.com>
2020-01-23 01:08:29 +0300

    change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./styles/header.css";
-import HeaderMenu from './HeaderMenu';
 
 
 const Header = (props) =>{
@@ -11,7 +10,19 @@ const Header = (props) =>{
                 <div className="header_logo">
                     <image alt="LOGO"></image>
                 </div>
-                <div className="header_menu"></div>
+                <div className="header_menu">
+                    <div className="header_menu_genre"><button className="header_menu_genre_button">ЖАНР</button></div>
+                    <div className="header_menu_tops"><button className="header_menu_tops_button">ЛУЧШЕЕ</button></div>
+                    <div className="header_menu_country"><button className="header_menu_country_button">СТРАНА</button></div>
+                    <div className="header_menu_news"><button className="header_menu_news_button">НОВИНКИ</button></div>
+                </div>
+                <div className="header_find">
+                    <div className="header_find_block"><input type="text" className="header_find_input"/></div>
+                </div>
+                <div className="header_menu_buttons">
+                    <div className="header_menu_buttons_In"><button className="header_menu_buttons_In_Button">ВХОД</button></div>
+                    <div className="header_menu_buttons_Reg"><button className="header_menu_buttons_Reg_Button">РЕГИСТРАЦИЯ</button></div>
+                </div>
             </div>);
 }
 
