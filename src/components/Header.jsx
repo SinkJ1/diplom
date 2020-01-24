18c124868fbf211f07e19fb8231799dc7c17728a
SinkJ <boi54357@gmail.com>
2020-01-24 19:55:26 +0300

    little change header menu buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = (props) =>{
                     </Link>
                 </div>
                 <div className="header_menu">
-                    <div className="header_menu_genre"><button className="header_menu_genre_button">ЖАНР<div className="genre_content"><button className="testBtn">hello</button></div></button></div>
+                    <div className="header_menu_genre"><button className="header_menu_genre_button">ЖАНР<div className="genre_content"><button className="testBtn">Комедия</button><button className="testBtn">Ужасы</button><button className="testBtn">Триллер</button></div></button></div>
                     <div className="header_menu_tops"><button className="header_menu_tops_button">ЛУЧШЕЕ<div className="genre_content"></div></button></div>
                     <div className="header_menu_country"><button className="header_menu_country_button">СТРАНА<div className="genre_content"></div></button></div>
                     <div className="header_menu_news"><button className="header_menu_news_button">НОВИНКИ<div className="genre_content"></div></button></div>
